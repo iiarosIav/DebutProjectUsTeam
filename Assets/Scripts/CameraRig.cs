0a91c75c9a5b2967cb35942c25c95fddf5e61f6a
Mega_Axolotl <cherkunovvv@gmail.com>
2024-06-11 11:57:31 +0200

    Player Movememt Beta
    Added scrol and camera movement

---
+++ Assets/Scripts/CameraRig.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class CameraRig : MonoBehaviour
+{
+    public Transform objectToFollow;
+    public float speed;
+
+    private Transform _transform;
+    public Transform Player;
+
+
+    public PlayerMove PlayerComponent;
+
+    public float KoefDistance;
+
+    [SerializeField] private float _maxDist;
+    [SerializeField] private float _minDist;
+
+
+    private void Awake()
+    {
+        _transform = transform;
+        _transform.position = objectToFollow.position;
+    }
+
+    private void FixedUpdate()
+    {
+        objectToFollow.LookAt(Player.position);
+        transform.LookAt(Player.position);
+        float mw = Input.GetAxis("Mouse ScrollWheel");
+        if (mw > 0.1 && Vector3.Distance(objectToFollow.transform.position, Player.transform.position) > _minDist)
+        {
+            objectToFollow.position += transform.forward * Time.deltaTime * KoefDistance;
+            PlayerComponent.MaxDistanceCounter();
+        }
+        else if (mw < -0.1 && Vector3.Distance(objectToFollow.transform.position, Player.transform.position) < _maxDist)
+        {
+            objectToFollow.position -= transform.forward * Time.deltaTime * KoefDistance;
+            PlayerComponent.MaxDistanceCounter();
+        }
+        
+        _transform.position = Vector3.Lerp(_transform.position, objectToFollow.position, Time.fixedDeltaTime * speed);
+    }
+}
+
--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditorInternal;
 using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
@@ -10,7 +11,12 @@ public class PlayerMove : MonoBehaviour
     [SerializeField] private Transform _playerModel;
     [SerializeField] private float _mouseSencetivity = 1f;
     [SerializeField] private Transform _cameraTransform;
+    [SerializeField] private Transform _camera;
     [SerializeField] private float _jumpSpeed;
+
+    [SerializeField] private GameObject _cameraRayPrefab;
+    
+    private float _maxDistance;
     
     private float _xAngle;
 
@@ -20,6 +26,7 @@ public class PlayerMove : MonoBehaviour
     {
         _rigidbody = GetComponent<Rigidbody>();
         Cursor.visible = false;
+        _maxDistance = (_camera.position - _cameraTransform.position).magnitude;
     }
 
     private void Update()
@@ -33,24 +40,22 @@ public class PlayerMove : MonoBehaviour
         float verticalInput = Input.GetAxis("Vertical");
 
         Vector3 inputVector = new Vector3(horizontalInput, 0, verticalInput);
-        Vector3 worldVelocity = _playerModel.TransformVector(inputVector) * speed;
+        Vector3 worldVelocity = _cameraTransform.TransformVector(inputVector) * speed;
+
+        if (inputVector != Vector3.zero)
+        {
+            _playerModel.rotation = Quaternion.LookRotation(inputVector);
+            _playerModel.localEulerAngles += new Vector3(0f, _cameraTransform.localEulerAngles.y, 0f);
+        }
         
         _rigidbody.velocity = new Vector3(worldVelocity.x, _rigidbody.velocity.y, worldVelocity.z);
-
-        float mouseX = Input.GetAxis("Mouse X"); 
-        float mouseY = Input.GetAxis("Mouse Y");
-
-        _playerModel.localEulerAngles += new Vector3(0f, mouseX * _mouseSencetivity, 0f);
-        
-        _xAngle -= mouseY * _mouseSencetivity;
-        _xAngle = Mathf.Clamp(_xAngle, -80, 80);
-
-        _cameraTransform.localEulerAngles = new Vector3(_xAngle, 0f, 0f);
 
         if (Input.GetKeyDown(KeyCode.Space) && _grounded)
         {
             _rigidbody.velocity += Vector3.up * _jumpSpeed;
         }
+
+        // CameraObstacleReact();
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -61,8 +66,29 @@ public class PlayerMove : MonoBehaviour
         }
     }
 
+    public void MaxDistanceCounter()
+    {
+        _maxDistance = (_camera.position - _cameraTransform.position).magnitude;
+    }
+
     private void OnCollisionExit(Collision collision)
     {
         _grounded = false;
+    }
+
+    private void CameraObstacleReact()
+    {
+        RaycastHit hit;
+        LayerMask layerMask = LayerMask.NameToLayer("Player");
+        float distance = Vector3.Distance(_camera.position, _playerModel.position);
+        if (Physics.Raycast(_cameraTransform.position, _camera.position - _cameraTransform.position, out hit,
+                _maxDistance, layerMask))
+        {
+            _camera.position = hit.point;
+        }
+        else if (distance < _maxDistance && !Physics.Raycast(_camera.position,-_camera.forward, .1f, layerMask))
+        {
+            _camera.position -= _camera.forward * .05f;
+        }
     }
 }
